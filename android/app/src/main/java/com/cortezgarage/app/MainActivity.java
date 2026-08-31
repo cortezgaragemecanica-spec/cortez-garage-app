79861565d074bcd94b038feba1f7e5ac64809b80
cortezgaragemecanica-spec <cortezgaragemecanica@gmail.com>
2026-08-30 22:10:37 -0300

    feat: adicionar notificacoes nativas da agenda

--- android/app/src/main/java/com/cortezgarage/app/MainActivity.java
+++ android/app/src/main/java/com/cortezgarage/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.cortezgarage.app;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
